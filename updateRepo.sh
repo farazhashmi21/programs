6dc8d5a6ef609b66aca54da5ff90fd11df6391c5
developerfaraz745 <muhammadfaraz745@gmail.com>
2021-06-08 20:30:05 +0500

    Program Updates

--- updateRepo.sh
+++ updateRepo.sh
@@ -1,7 +1,6 @@
-echo $(echo "# programs" >> README.md);
 echo $(git init);
 echo $(git add .);
-echo $(git commit -m "first commit");
+echo $(git commit -m "Program Updates");
 echo $(git branch -M main);
 echo $(git remote -v);
 echo $(git push -u origin main);
