3e264df7dc77ff2f92291663f3dcba51e6467108
developerfaraz745 <muhammadfaraz745@gmail.com>
2021-07-03 19:40:13 +0500

    Program Updates

--- python/variables.py
+++ python/variables.py
@@ -2,10 +2,57 @@
 
 # Python Programming Variables Example.
 
-class variab:
-  def displ(self):
-    print("Working")
+x = 5
+y = "Jone"
+print(x)
+print(y)
 
-v = variab();
-v.displ()
+x = True
+print(x)
+y = str(46.64)
+print(y)
+#x = int(y)
+print(type(y))
+p = 'Pakistan'
+print(p)
+K = "Karachi"
+print(K)
+stat = "I lived in " + K + ", " + p + "."
+print(stat)
+
+#Unpacking Variables
+
+x, y, z = ['a','b','c']
+print(x)
+print(y)
+print(z)
+one = "I am learning"
+two = "Python Programming Language"
+print(one + " " + two + ".")
+
+one = 46.53
+two = 45.32
+sum = one + two
+print(sum)
+
+new = "Love"
+def displ():
+  print(new + " Pakistan")
+displ()
+def add():
+  x, y = 20, 40
+  sum = x + y
+  return sum
+print("Answer: " + str(add()) + ".")
+
+var = 20
+num = 34
+def multi():
+  global var
+  var = 2
+  global num
+  num  = 8
+  result = var + num
+  print(result)
+multi()
 
