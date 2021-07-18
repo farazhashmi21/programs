f485f617f06b3aac095b4f31e9203c9fa290288f
farazhashmi21 <developerfaraz@yahoo.com>
2021-07-18 12:03:12 +0100

    Program Updates

--- python/lists.py
+++ python/lists.py
@@ -25,3 +25,22 @@ print(numbers)
 print(len(numbers))
 MyCountry = list(("Pakistan","Karachi","North Naizamabad","74700"))
 print(MyCountry)
+
+NewList = ["Pakistan", 'China', "Turkey"]
+print(NewList[0])
+print(NewList[1])
+print(NewList[2])
+print("Reverse Indexing")
+print(NewList[-2])
+print(NewList[-1])
+print(NewList[-0])
+
+fruits = ["apples", "Oranges","Mangos","Grapes","Bananas","Strawberries","Blueberries","Kiwi"]
+FruitS = fruits[-7:-2]
+print(fruits)
+print(FruitS)
+print(fruits[:7])
+print(fruits[:-7])
+print(fruits[2:5])
+if "Strawberries" in fruits:
+  print("YES it is present in the list @ position no.: " + str(fruits.index("Strawberries")))
