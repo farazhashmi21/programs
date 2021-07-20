605c0db44097ae76fdff911ef4f02a0215947406
developerfaraz745 <muhammadfaraz745@gmail.com>
2021-07-20 16:30:07 +0500

    Program Updates

--- python/pracs/bin/activate.fish
+++ python/pracs/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/farazhashmi/Desktop/programs/python/pracs"
+set -gx VIRTUAL_ENV "/home/farazhashmi/programs/python/pracs"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
