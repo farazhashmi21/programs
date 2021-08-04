bfa45442117a2fe5e476b03033c5c6ec63c63ffb
developerfaraz745 <muhammadfaraz745@gmail.com>
2021-08-04 17:36:30 +0500

    Wordpress Updates

--- updateRepo.sh
+++ updateRepo.sh
@@ -1,7 +1,7 @@
 echo $(git pull);
 echo $(git init);
 echo $(git add .);
-echo $(git commit -m "Program Updates");
+echo $(git commit -m "Wordpress Updates");
 echo $(git branch -M main);
 echo $(git remote -v);
 echo $(git push -u origin main);
