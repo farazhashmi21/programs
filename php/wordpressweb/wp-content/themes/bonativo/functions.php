9326d9b271a8f3a6d9d34c4b30b8b3db5c478841
developerfaraz745 <muhammadfaraz745@gmail.com>
2021-08-04 20:10:12 +0500

    Folder Updates

---
+++ php/wordpressweb/wp-content/themes/bonativo/functions.php
@@ -0,0 +1,4 @@
+<?php
+  // functions.php
+  register_nav_menus(array('primary_menu' => 'Main Menu','secondary_menu' => 'Social Menu'));
+?>
--- php/wordpressweb/wp-content/themes/bonativo/index.php
+++ php/wordpressweb/wp-content/themes/bonativo/index.php
@@ -1,39 +1,4 @@
-<?php
-#  echo get_template_directory_uri();
-#  echo "<br/>";
-#  echo bloginfo("template_directory");
-?>
-<!DOCTYPE html>
-<!--
-	Bonativo by TEMPLATE STOCK
-	templatestock.co @templatestock
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
--->
-
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title><?php bloginfo('name'); ?></title>
-
-        <!-- Bootstrap -->
-        <link href="<?php echo get_template_directory_uri(); ?>/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-        <link href="<?php echo get_template_directory_uri(); ?>/css/style.css" rel="stylesheet" type="text/css">
-        <link href="<?php echo get_template_directory_uri(); ?>/css/flexslider.css" rel="stylesheet" type="text/css">
-        <link href="<?php echo get_template_directory_uri(); ?>/icons/css/ionicons.min.css" rel="stylesheet" type="text/css">
-        <link href="<?php echo get_template_directory_uri(); ?>/icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
-       
-        <!--revolution slider setting css-->
-        <link href="<?php echo get_template_directory_uri(); ?>/rs-plugin/css/settings.css" rel="stylesheet">
-        <link href="<?php echo get_template_directory_uri(); ?>/css/prettyPhoto.css" rel="stylesheet" type="text/css" />
-        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-        <![endif]-->
-    </head>
+<?php get_header(); ?>
     <body data-spy="scroll" data-target=".navbar" data-offset="80">
 
 
@@ -50,23 +15,7 @@
                     <a class="navbar-brand alo" href="index.html">Bonativo</a>
                 </div>
                 <div id="navbar" class="navbar-collapse collapse">
-                    <ul class="nav navbar-nav navbar-right scroll-to">
-                        <li class="active"><a href="#home">Home</a></li>
-                        <li><a href="#about">About</a></li>
-                        <li><a href="#services">Services</a></li>
-                        <li><a href="#work">Work</a></li>
-                        <li><a href="#blog">Blog</a></li>
-                        <li><a href="post-single.html">Blog Post</a></li>                      
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">Pages <span class="caret"></span></a>
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="typography.html">Typography</a></li>
-                                
-                            </ul>
-                        </li>
-                        <li><a href="#contact">Contact</a></li>
-
-                    </ul>
+                    <?php wp_nav_menu(array('primary_menu', 'menu_class' => 'navbar navbar-default navbar-fixed-top before-color')); ?>
                 </div><!--/.nav-collapse -->
             </div><!--/.container-fluid -->
         </nav>
@@ -784,31 +733,4 @@
             </div>
         </section><!--contact section end-->
 
-        <footer class="footer">
-            <div class="container text-center">
-                <span class="alo">Bonativo</span>
-                <ul class="social list-inline">
-                    <li><a href="#"><i class="icon icon-social-twitter"></i></a></li>
-                    <li><a href="#"><i class="icon icon-social-facebook"></i></a></li>
-                    <li><a href="#"><i class="icon icon-social-dribbble"></i></a></li>
-                </ul>
-                <span class="copyright">&copy; Copyright 2016. Bonativo Created By <a href="http://templatestock.co">Template Stock</a></span>
-            </div>
-        </footer>
-        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-        <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.min.js" type="text/javascript"></script>
-        <script src="<?php echo get_template_directory_uri(); ?>/js/moderniz.min.js" type="text/javascript"></script>
-        <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.easing.1.3.js" type="text/javascript"></script>
-        <!-- bootstrap js-->
-        <script src="<?php echo get_template_directory_uri(); ?>/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-        <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.flexslider-min.js"></script>
-        <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/parallax.min.js"></script> 
-        <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.prettyPhoto.js"></script>	       
-        <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jqBootstrapValidation.js"></script>
-        <!--revolution slider scripts-->
-        <script src="<?php echo get_template_directory_uri(); ?>/rs-plugin/js/jquery.themepunch.tools.min.js" type="text/javascript"></script>
-        <script src="<?php echo get_template_directory_uri(); ?>/rs-plugin/js/jquery.themepunch.revolution.min.js" type="text/javascript"></script>  
-        <script src="<?php echo get_template_directory_uri(); ?>/js/template.js" type="text/javascript"></script>
-
-    </body>
-</html>
+<?php get_footer(); ?>
